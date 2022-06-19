b2b22efd7e8a47ebc84cffff0e0661af46f5fbf1
Henrique Luiz <heenluy@gmail.com>
2022-06-19 16:18:31 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Projeto
+Explorando o CDK de acessibilidade do Angular. [Codelab](https://codelabs.developers.google.com/angular-a11y#0)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { Title } from '@angular/platform-browser';
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LocationComponent } from './location/location.component';
+import { ShopComponent } from './shop/shop.component';
+
+// TODO: #4. Define unique page titles
+const routes: Routes = [
+  { path: 'shop', component: ShopComponent, title: 'Our Shop – a11y in Angular'},
+  { path: 'about', component: AboutComponent, title: 'Our Story – a11y in Angular' },
+  { path: 'locate', component: LocationComponent, title: 'Find Us – a11y in Angular' },
+  { path: '',   redirectTo: '/shop', pathMatch: 'full' },
+  { path: '**', component: ShopComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
